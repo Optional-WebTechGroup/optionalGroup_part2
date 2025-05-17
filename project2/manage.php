ce4c9f8fbd2f03def30cf23fe6ebe387d0dc1932
Oliver Scott <105912692@student.swin.edu.au>
2025-05-17 18:47:22 +1000

    added all/seclect textbox and submit button basic

--- project2/manage.php
+++ project2/manage.php
@@ -18,7 +18,16 @@ basic html structure
 	<?php include_once 'header.inc'; ?>
     
     <main>
-        <p>This is a basic HTML template with meta tags, a header, and a body section.</p>
+        <h1>Manager Profile</h1>
+
+        <form method="post" action="">
+            <section>
+                <input type="text" name="EOI_reference" id="EOI_reference" required maxlength="20">
+                <input id="submit_button" type="submit" value="Apply">
+            </section>
+        </form>
+        <input id="submit_button" type="submit" value="Apply">
+
     </main>
 	
     <?php include_once 'footer.inc'; ?>
