704bc72998073c27fcd2d9fa024585202b9cb7e9
Aldrin Filbert Maano <105667785@student.swin.edu.au>
2025-05-17 22:48:50 +1000

    added sanitize input function

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -5,4 +5,13 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: apply.php');
     exit();
 }
+
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = strip_tags($data);
+    return $data;
+}
+
+
 ?>
