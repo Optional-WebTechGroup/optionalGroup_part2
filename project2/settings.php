1ca01e6c831fc5ba81e795e6c0dcb6c46eec677d
Nikolas Popovic <105920284@student.swin.edu.au>
2025-05-21 15:41:24 +1000

    Made the signup and login pages, w/ code

--- project2/settings.php
+++ project2/settings.php
@@ -3,4 +3,11 @@ $host = "localhost";
 $user = "root";
 $pwd = "";
 $sql_db = "optional_group_db";
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
 ?>
